9197f80e4c082ed497b8151f1ff72caf882d20e2
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2020-11-01 20:20:23 -0500

    Repertoire : ajout d’une liste de produits et méthodes qui vont avec

--- src/epicerie/Repertoire.java
+++ src/epicerie/Repertoire.java
@@ -3,16 +3,24 @@ package epicerie;
 import java.util.ArrayList;
 
 public class Repertoire {
-	private static ArrayList<Personne> repertoire = new ArrayList<Personne>();
+	private static ArrayList<Personne> personnes = new ArrayList<Personne>();
+	private static ArrayList<Produit> produits = new ArrayList<Produit>();
 	
 	public void add(Personne personne) {
-		repertoire.add(personne);
+		personnes.add(personne);
+	}
+
+	public void add(Produit produit) {
+		produits.add(produit);
 	}
 
 	public String toString() {
 		String result = "";
-		for(Personne personne : repertoire) {
+		for(Personne personne : personnes) {
 			result += personne.toString() + "\n";
+		}
+		for(Produit produit : produits) {
+			result += produit.toString() + "\n";
 		}
 		return result;
 	}
@@ -20,7 +28,7 @@ public class Repertoire {
 	public static int searchEmail(String debutCourriel) {
 		int compteur = 0;
 		
-		for(Personne personne : repertoire) {
+		for(Personne personne : personnes) {
 			String pCourriel = personne.getCourriel();
 			
 			// On remplace 0 ou plus chiffres (d*), le @ 
@@ -30,27 +38,33 @@ public class Repertoire {
 			if ( debutCourriel.equals(pCourriel)) {
 				compteur++;
 			}			
-		}
-		
+		}	
 		return compteur;
 	}
 	
 	public int indexOf(Personne personne) {
-		return repertoire.indexOf(personne);
+		return personnes.indexOf(personne);
+	}
+	
+	public int indexOf(Produit produit) {
+		return produits.indexOf(produit);
 	}
 	
 	public boolean remove(Personne personne) {
-		return repertoire.remove(personne);
+		return personnes.remove(personne);
+	}
+	
+	public boolean remove(Produit produit) {
+		return produits.remove(produit);
 	}
 	
 	public ArrayList<Personne> search(String searchedString) {
 		ArrayList<Personne> liste = new ArrayList<Personne>();
-		for(Personne personne : repertoire) {
+		for(Personne personne : personnes) {
 			if (personne.getNom().startsWith(searchedString)) {
 				liste.add(personne);
 			}			
 		}
 		return liste;
 	}
-
 }

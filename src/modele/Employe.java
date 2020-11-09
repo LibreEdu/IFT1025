54d6d4e2a076e4acf32b3029fd7131331c078a65
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2020-11-09 08:20:38 -0500

    Employe : remove toString + augmenterSolde(Client)

--- src/modele/Employe.java
+++ src/modele/Employe.java
@@ -5,14 +5,6 @@ public class Employe extends Personne{
 	public Employe(String prenom, String nom, String date) {
 		super(prenom, nom, date);
 	}
-
-//	public String toString() {
-//		return super.toString();
-//	}
-	
-//	public void augmenterSolde(Client client, double montant) {
-//		client.addMoney((float)montant);
-//	}
 	
 	public void substractMoney(float solde) {
 		this.solde -= solde;
